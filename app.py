11e34ce4dc432cec49b2194ac94e7eb80b4712a4
Alexandre Carvalho <alexandre.carvalho@stenn.com>
2024-11-21 16:20:28 +0000

    adds snowflake library and changes app

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import streamlit as st
 import pandas as pd
+import modules.sf_handler as sh
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
+logging.getLogger("snowflake.connector").setLevel(logging.WARNING)
+
+def process_input(user_input):
+    conn = sh.get_connection()
+    logging.info("Connection established.")
+    df = sh.run_query(conn, f"select * from {user_input}", 'df')
+    return df
 
 # Title of the app
-st.title("CSV File Uploader")
+st.title("Data Fetcher")
 
-# Sidebar for file upload
-st.sidebar.header("Upload CSV file")
-uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type=["csv"])
+st.sidebar.header("Input Section")
+user_input = st.sidebar.text_input("Enter a value:")
+submit_button = st.sidebar.button("Submit")
 
-# Check if a file is uploaded
-if uploaded_file is not None:
-    # Read the CSV file
-    df = pd.read_csv(uploaded_file)
-    
-    # Display the dataframe
-    st.write(df)
-else:
-    st.write("Please upload a CSV file.")
-
+if submit_button:
+    if user_input:
+        try:
+            df = process_input(user_input)
+            if not df.empty:
+                st.table(df)
+            else:
+                st.info("The query returned no data.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    else:
+        st.error("Please enter a value in the text field.")
